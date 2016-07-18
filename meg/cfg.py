5e9265bcf32c26ab1e6bb8ef3f7be5a999935af8
Joshua Lyle <jlyle@anl.gov>
2016-07-18 10:40:31 -0500

    comments

--- meg/cfg.py
+++ meg/cfg.py
@@ -5,31 +5,44 @@ from celery import Celery
 import configmaster
 
 
-def get_yml_config():
-    file_dir = os.path.dirname(__file__)
-    meg_config = os.getenv("MEG_SERVER_CFG", default="{}/config.yml".format(file_dir))
-    cfg = configmaster.YAMLConfigFile.YAMLConfigFile(meg_config)
-    meg_default_config = os.getenv("MEG_SERVER_DEFAULT_CFG", default="{}/config.default.yml".format(file_dir))
-    cfg.apply_defaults(configmaster.YAMLConfigFile.YAMLConfigFile(meg_default_config))
-    return cfg
-
-
 def configure_app(app, testing, debug):
+    # Save debug and testing settings
     app.debug = debug
     app.config["TESTING"] = testing
+
+    # Load configs from user
     cfg = get_yml_config()
+
+    #
     configure_db(app, cfg, testing)
     celery = create_and_configure_celery(app)
     dictConfig(cfg.config.logging.dump())
     return cfg, celery
 
 
+def get_yml_config():
+    # Get user config and default config paths
+    file_dir = os.path.dirname(__file__)
+    meg_config = os.getenv("MEG_SERVER_CFG", default="{}/config.yml".format(file_dir))
+    meg_default_config = os.getenv("MEG_SERVER_DEFAULT_CFG", default="{}/config.default.yml".format(file_dir))
+
+    # Load user configuration. Use default config for non-specified items
+    cfg = configmaster.YAMLConfigFile.YAMLConfigFile(meg_config)
+    cfg.apply_defaults(configmaster.YAMLConfigFile.YAMLConfigFile(meg_default_config))
+
+    return cfg
+
+
 def configure_db(app, cfg, testing):
+    # If not using the unit tests, use the postgres database
     if not testing:
         uri = "postgresql://{user}:{password}@{host}:{port}/{database}?client_encoding=utf8".\
             format(**cfg.config.db)
+    # Otherwise use the SQLite database
     else:
         uri = "sqlite://"
+
+    #
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
